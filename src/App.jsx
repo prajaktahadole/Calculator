a0fa5e0a5c90df23cdc9614d090a61071e61e52e
prajaktahadole <prajaktahadole@gmail.com>
2022-11-07 23:57:21 +0530

    keypad

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,41 @@ import './App.css'
 
 function App() {
 
+  const Numbers = () =>{
+    const numbs = [];
+    for(let i = 9; i >= 0; i--)
+    {
+      numbs.push(
+        <button key={i}>{i}</button>
+      )
+    }
+    return numbs;
+  }
+
+
   return (
     <div className="App">
-      Calculator
+      <div className='calculator'>
+        <div className='display'></div>
+        
+        <div >
+                  <button>AC</button>
+                  <button>+/-</button>
+                  <button>%</button>
+                  {Numbers()}
+                  <button>.</button>
+        </div>
+
+        <div>
+                  <button>/</button>
+                  <button>*</button>
+                  <button>+</button>
+                  <button>-</button>
+                  <button>=</button>
+        </div>
+                
+
+      </div>
     </div>
   )
 }
