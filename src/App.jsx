96e0f013407c5c04447d9b13e1acf54ebbe8de95
prajaktahadole <prajaktahadole@gmail.com>
2022-11-08 17:54:15 +0530

    display

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import './App.css'
+import { useState } from 'react';
 
 function App() {
+
+  const [cal , setCal] = useState("");
+  const [result, setResult] = useState("");
+
+  const operators = ['÷' , 'x' , '+' , '-', '+-', '%' , '.']
+
+  const UpdateCal = value =>{
+    setCal(cal+value )
+  }
 
   const Numbers = () =>{
     const numbs = [];
     for(let i = 9; i >= 0; i--)
     {
       numbs.push(
-        <button key={i}>{i}</button>
+        <button onClick={() => UpdateCal(i.toString())} 
+        key={i}>{i}
+         </button>
       )
     }
     return numbs;
@@ -18,23 +30,24 @@ function App() {
     <div className="App">
       <div className='calculator'>
         <div className='display'>
-          <span>0</span>
+          {result ? <span>(0)</span> : '' }
+          {cal || '0'}
         </div>
         
       <div className='keypad'>
         <div className='left'>
                   <button>AC</button>
-                  <button>+-</button>
-                  <button>%</button>
+                  <button onClick={() => UpdateCal('+-')}>+-</button>
+                  <button onClick={() => UpdateCal('%')}>%</button>
                   {Numbers()}
-                  <button>.</button>
+                  <button onClick={() => UpdateCal('.')}>.</button>
         </div>
 
         <div className='right'> 
-                  <button>÷</button>
-                  <button>x</button>
-                  <button>+</button>
-                  <button>-</button>
+                  <button onClick={() => UpdateCal('÷')}>÷</button>
+                  <button onClick={() => UpdateCal('x')}>x</button>
+                  <button onClick={() => UpdateCal('+')}>+</button>
+                  <button onClick={() => UpdateCal('-')}>-</button>
                   <button className='equal'>=</button>
         </div>
         </div>       
